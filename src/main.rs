5b91b8ac82501b3aa4d82cb577fb6035b1814a42
Debadree Chatterjee <debadree333@gmail.com>
2024-03-09 13:45:57 +0530

    improve code a bit

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::os::unix::io::{AsRawFd, RawFd};
+use threadpool::ThreadPool;
 
 #[allow(unused_macros)]
 macro_rules! syscall {
@@ -23,9 +24,22 @@ struct RequestContext {
 
 impl RequestContext {
     fn new(stream: TcpStream) -> RequestContext {
-        RequestContext {
-            stream,
+        RequestContext { stream }
+    }
+
+    fn handle_read(&mut self) -> Result<Option<String>, Box<dyn std::error::Error>> {
+        let mut buffer = [0; 1024];
+        let nread = self.stream.read(&mut buffer)?;
+        if nread == 0 {
+            Ok(None)
+        } else {
+            Ok(Some(std::str::from_utf8(&buffer)?.to_string()))
         }
+    }
+
+    fn handle_write(&mut self, response: &str) -> std::io::Result<()> {
+        self.stream.write_all(response.as_bytes())?;
+        Ok(())
     }
 }
 
@@ -89,30 +103,60 @@ fn main() {
     let listener_fd = listener.as_raw_fd();
     let kq = kqueue().expect("Failed to create kqueue");
     let mut streams_map = HashMap::new();
-    register_with_kqueue_for_read(kq, listener_fd).expect("Failed to register listener with kqueue");
+    register_with_kqueue_for_read(kq, listener_fd)
+        .expect("Failed to register listener with kqueue");
+    let pool = ThreadPool::new(4);
     loop {
         println!("Waiting for events");
+        println!("Requests in flight {}", streams_map.len());
         let events = get_kqueue_events(kq).expect("Failed to get kqueue events");
         println!("Got {} events", events.len());
         for event in events {
             if event.ident == listener_fd as usize {
-                let (stream, _) = listener.accept().expect("Failed to accept connection");
-                stream.set_nonblocking(true).expect("Failed to set non-blocking mode on stream");
-                let fd = stream.as_raw_fd();
-                register_with_kqueue_for_read(kq, fd).expect("Failed to register stream with kqueue");
-                streams_map.insert(fd, RequestContext::new(stream));
+                match listener.accept() {
+                    Ok((stream, _)) => {
+                        stream
+                            .set_nonblocking(true)
+                            .expect("Failed to set non-blocking mode on stream");
+                        let fd = stream.as_raw_fd();
+                        register_with_kqueue_for_read(kq, fd)
+                            .expect("Failed to register stream with kqueue");
+                        streams_map.insert(fd, RequestContext::new(stream));
+                    }
+                    Err(e) => {
+                        if e.kind() != std::io::ErrorKind::WouldBlock {
+                            eprintln!("Failed to accept connection: {}", e);
+                        }
+                    }
+                }
             } else {
                 let fd = event.ident as i32;
-                let request_context = streams_map.get_mut(&fd).expect("Failed to get request context");
-                let mut buffer = [0; 1024];
-                request_context.stream.read(&mut buffer).expect("Failed to read from stream");
-                std::str::from_utf8(&buffer)
-                    .expect("Failed to convert buffer to string")
-                    .split("\r\n")
-                    .for_each(|line| {
-                        println!("Got line: {}", line);
-                    });
-                request_context.stream.write_all(b"+PONG\r\n").expect("Failed to write to stream");
+                match streams_map.get_mut(&fd) {
+                    Some(request_context) => {
+                        if event.filter == libc::EVFILT_READ {
+                            match request_context.handle_read() {
+                                Ok(Some(request)) => {
+                                    println!("Got request: {}", request);
+                                    request_context
+                                        .handle_write("+PONG\r\n")
+                                        .unwrap_or_else(|e| {
+                                            eprintln!("Failed to write response: {}", e)
+                                        })
+                                }
+                                Ok(None) => {
+                                    println!("Client disconnected");
+                                    streams_map.remove(&fd);
+                                }
+                                Err(e) => {
+                                    eprintln!("Failed to read from stream: {}", e);
+                                }
+                            }
+                        }
+                    }
+                    None => {
+                        eprintln!("Got event for unknown file descriptor: {}", fd);
+                    }
+                }
             }
         }
     }
